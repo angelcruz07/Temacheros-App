5f0f2035068d6dbfd7a1b41ef4847e9f04004efc
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-07 14:40:57 -0600

    Add globals.css, Add mockup of home

---
+++ src/components/ui/navbarHome/button/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+	return <div>This a button</div>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,25 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'bg-deg': '#060606',
-        'bg-deg-2': '#5D5D5D',
-        'color-mayus': '#F00404',
-        'color-bg-component': '#888888',
-        'color-bg-button': '#888888',
-        'color-bg-button-2': '#5D5D5D',
-        'color-text-advertisement': '#888888',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				'bg-deg': '#060606',
+				'bg-deg-2': '#5D5D5D',
+				'color-red': '#F00404',
+				'color-bg-component': '#888888',
+				'color-bg-button': '#888888',
+				'color-bg-button-2': '#5D5D5D',
+				'color-text-advertisement': '#888888'
+			}
+		}
+	},
+	plugins: []
 }
+
 export default config
