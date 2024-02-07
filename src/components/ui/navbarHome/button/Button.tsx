7ac50de6ad5963f4f03e5ec886428a127eae34f7
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-07 17:19:38 -0600

    Add Cursor pointer in button

--- src/components/ui/navbarHome/button/Button.tsx
+++ src/components/ui/navbarHome/button/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonInterface } from '@/interfaces/'
 
 export const Button = ({ url, text }: ButtonInterface) => {
 	return (
-		<div className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center'>
+		<div className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer'>
 			<Link href={url}>{text}</Link>
 		</div>
 	)
