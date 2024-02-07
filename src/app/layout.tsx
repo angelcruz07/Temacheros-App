8a03b682d000b96564dbbeaa443c5e9a0523cb24
ariff-dev <ariff.dev24@gmail.com>
2024-02-07 15:07:21 -0600

    Task completed: Background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css'
+
 import { Metadata } from 'next'
 
 import { Inter } from 'next/font/google'
@@ -17,7 +19,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='es'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-gradient-to-br from-bg-deg  from-45% to-bg-deg-2 min-h-screen text-white`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
