22f42d69971e66bce7a5d82ae097b0e4f2de65b0
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-09 16:20:33 -0600

    Add refactor was added to the site mapping data in the 'data' directory.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,49 +1,24 @@
 import { Button } from '@/components'
-// improve in the future
-const buttonsData = [
-	{
-		id: 1,
-		url: '/',
-		text: 'Comenzar'
-	},
-	{
-		id: 2,
-		url: '/',
-		text: 'Comunidad'
-	},
-	{
-		id: 3,
-		url: '/mammoth',
-		text: 'Cabezas de mamut'
-	},
-	{
-		id: 4,
-		url: '/',
-		text: 'Registros de la tribu'
-	}
-]
+import { buttonsData } from '@/data'
 
 export default function page() {
 	return (
 		<section className='flex flex-col p-5'>
 			<h1 className='text-lg md:text-3xl font-bold text-center '>
-				El Modo
-				<span className='text-color-mayus'> Guerra</span>
+				El Modo <span className='text-color-mayus'> Guerra</span>
 			</h1>
-			<div className='flex flex-col md:flex-row justify-center items-center md:gap-x-20 md:mt-14 mt-20'>
-				<article>
-					<picture className='md:w-full md:h-full w-52 h-52'>
-						<img
-							src='/assets/images/Temach.webp'
-							alt="Luis castilleja 'El Temach'"
-						/>
-						<h2 className='text-center p-5'>
-							Luis Castilleja{' '}
-							<span className='text-color-mayus font-bold'>"El Temach"</span>
-						</h2>
-					</picture>
-				</article>
-				<article className='md:max-w-2/5'>
+			<article className='flex flex-col md:flex-row justify-center items-center md:gap-x-20 md:mt-14 mt-20'>
+				<picture className='md:w-full md:h-full w-52 h-52'>
+					<img
+						src='/assets/images/Temach.webp'
+						alt="Luis castilleja 'El Temach'"
+					/>
+					<h2 className='text-center p-5'>
+						Luis Castilleja{' '}
+						<span className='text-color-mayus font-bold'>"El Temach"</span>
+					</h2>
+				</picture>
+				<div className='md:max-w-2/5'>
 					<p className='text-lg font-bold md:text-2xl text-center'>
 						Sumergete en el contenido del
 						<span className='text-color-mayus'> Temach©</span>, enfocado al
@@ -54,8 +29,8 @@ export default function page() {
 							<Button key={button.text} url={button.url} text={button.text} />
 						))}
 					</div>
-				</article>
-			</div>
+				</div>
+			</article>
 		</section>
 	)
 }
---
+++ src/data/data-buttons-menu/buttonMenu.data.ts
@@ -0,0 +1,22 @@
+export const buttonsData = [
+	{
+		id: 1,
+		url: '/',
+		text: 'Comenzar'
+	},
+	{
+		id: 2,
+		url: '/',
+		text: 'Comunidad'
+	},
+	{
+		id: 3,
+		url: '/mammoth',
+		text: 'Cabezas de mamut'
+	},
+	{
+		id: 4,
+		url: '/',
+		text: 'Registros de la tribu'
+	}
+]
