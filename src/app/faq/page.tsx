da7c76482085ef9e379ed234aedb211879d92dd5
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-14 14:39:07 -0600

    Add CardFaq component and data-faq module

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,15 @@
+import { CardFaq } from '@/components'
+import { DataFaq } from '@/data'
+
 export default function Faq() {
 	return (
-		<section>
+		<section className='h-screen'>
 			<h1 className='text-3xl font-bold'>Preguntas frecuentes</h1>
+			<article className='mt-5 flex gap-x-5 flex-col md:flex-row'>
+				{DataFaq.map((questions) => (
+					<CardFaq key={questions.id} {...questions} />
+				))}
+			</article>
 		</section>
 	)
 }
