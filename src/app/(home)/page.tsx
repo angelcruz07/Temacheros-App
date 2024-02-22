ca3b8cde04b1efb11aa5f91442cf581423d6031c
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-21 19:42:58 -0600

    refactor: add folder for home-menu | dev: angelcruz07

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,4 @@
-// Icons Library
-import {
-	IconFolderOpen,
-	IconHelpHexagon,
-	IconHome,
-	IconNorthStar,
-	IconUsers
-} from '@tabler/icons-react'
-import Link from 'next/link'
+import { HomeMenu } from '@/components'
 
 export default function page() {
 	return (
@@ -31,39 +23,7 @@ export default function page() {
 						<span className='text-color-mayus'> Temach©</span>, enfocado al
 						estudio y superación personal.
 					</p>
-					<div className='flex justify-center flex-col gap-y-3 items-center mt-4'>
-						{/* ? links to navbar */}
-						<Link
-							href='/start'
-							className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer flex items-center justify-center'>
-							<IconHome />
-							Comenzar
-						</Link>
-						<Link
-							href='/start'
-							className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer flex items-center justify-center'>
-							<IconFolderOpen />
-							Recursos
-						</Link>
-						<Link
-							href='/start'
-							className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer flex items-center justify-center'>
-							<IconUsers />
-							Comunidad
-						</Link>
-						<Link
-							href='/start'
-							className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer flex items-center justify-center'>
-							<IconNorthStar />
-							Cabezas de mammut
-						</Link>
-						<Link
-							href='/start'
-							className='bg-color-bg-component p-3 rounded-lg w-80 md:w-96 text-center cursor-pointer flex items-center justify-center'>
-							<IconHelpHexagon />
-							Preguntas frecuentes
-						</Link>
-					</div>
+					<HomeMenu />
 				</div>
 			</article>
 		</section>
